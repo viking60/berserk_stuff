4274bb033604e64bef403686756ac4a69029b6dd
viking60 <thomas@bjoernvold.com>
2014-10-29 19:49:57 +0100

    v01.1

--- maps/genlist.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for i in *.ktl
-do
-	lang=$(echo $i | sed -e 's!.ktl!!g')
-	echo "label $lang"
-	echo "  kernel kbdmap.c32"
-	echo "  append maps/$i"
-done
--- plugins/maui.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-if [ $1 = links ];then
-	echo "maui-*.iso maui.fedora.iso Maui_(*)"
-fi
-
